0afdd3ad532c852d2f220ca7aa4387492439a59a
clun <cedrick.lunven@gmail.com>
2023-04-20 12:55:35 +0200

    code

--- src/main/java/com/datastax/astra/AstraDriver4x.java
+++ src/main/java/com/datastax/astra/AstraDriver4x.java
@@ -3,14 +3,13 @@ import java.nio.file.Paths;
 import com.datastax.oss.driver.api.core.CqlSession;
 
 public class AstraDriver4x {
-
-    public static void main(String[] args) {
-        try (CqlSession cqlSession = CqlSession.builder()
-                .withCloudSecureConnectBundle(Paths.get("/tmp/secure-connect-bundle-db-demo."))
-                .withAuthCredentials("client_id","client_secret")
-                .withKeyspace("keyspace_demo")
-                .build()) {
-            System.out.println("Hello keyspace {} !" + cqlSession.getKeyspace().get());
-        }
+  public static void main(String[] args) {
+    try (CqlSession cqlSession = CqlSession.builder()
+      .withCloudSecureConnectBundle(Paths.get("/tmp/secure-connect-bundle-db-demo."))
+      .withAuthCredentials("client_id","client_secret")
+      .withKeyspace("keyspace_demo")
+      .build()) {
+       System.out.println("Connected to " + cqlSession.getKeyspace().get());
     }
+  }
 }
